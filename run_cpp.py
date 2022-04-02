e4267f0c770c25b4bc6fefcba2476483ee9ded1b
Abhishek Pandey <70065781+Abhi-dev007@users.noreply.github.com>
2022-04-02 14:58:59 +0530

    updated run_cpp.py

--- run_cpp.py
+++ run_cpp.py
@@ -1,9 +1,14 @@
 # code for running cpp file of CIDLPA algorithm
+import os
 import subprocess
 
 def run_cpp(cpp_file, exe_file):
-    x = subprocess.getoutput('g++' + cpp_file + '-o' + exe_file)
+    cwd = os.getcwd()
+    cwd1 = cwd + "\\test"
+    os.chdir(cwd1)
+    x = subprocess.getoutput('g++ ' + cpp_file + ' -o ' + exe_file)
     if x == "":
         subprocess.run(exe_file)
     else:
         print(x)
+    os.chdir(cwd)
